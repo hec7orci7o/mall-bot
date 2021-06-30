e4e40abc471323303631b665fb79bd56ca60e738
Hector Toral <hectortoraltrabajos@gmail.com>
2021-06-30 10:47:12 +0200

    avance en cogs.bartender

--- src/cogs/bartender.py
+++ src/cogs/bartender.py
@@ -1,9 +1,54 @@
+import random
+import asyncio
 import discord
 from discord.ext import commands
+from libs.database import DataBase
 
 class Bartender(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.database = DataBase()
+
+    @commands.command(name = 'order')
+    async def get_product(self, ctx, val):
+        # Check producto registrado
+        sql = """SELECT nombre FROM productos WHERE nombre = '{}';""".format(val.lower())
+        self.database.cursor.execute(sql)
+        result = self.database.cursor.fetchall()
+
+        if result == []:
+            await ctx.send("Sry, there are not products registered with the name: {}".format(val.lower()))
+        else:
+            # Check disponibilidad del producto registrado
+            sql = """SELECT nombre, url FROM imagenes WHERE nombre = '{}';""".format(val.lower())
+            self.database.cursor.execute(sql)
+            result = self.database.cursor.fetchall()
+
+            if result == []:
+                await ctx.send("Sry, there are not enought {}".format(val.lower()))
+            else:
+                await self.get_item(ctx, val.lower(), result)
+
+    async def get_item(self, ctx, val, result):
+        await ctx.trigger_typing()
+        embed = discord.Embed(
+            title = f"Please wait while I prepare your order...",
+            color = "#fff"
+        )
+        message = await ctx.send(embed = embed)
+        
+        await ctx.trigger_typing()
+        await asyncio.sleep(5)
+        embed = discord.Embed (
+            title = f"Here is your {}, sir.",
+            color = "#fff"
+        )
+        url = # seleccionar url de una tupla aleatoria entre todas las de la lista
+        embed.set_image(url=url)
+        await message.delete()
+        message = await ctx.send(embed = embed)
+        for reaction in ['👍', '👎']:
+            await message.add_reaction(reaction)
     
 def setup(bot):
     bot.add_cog(Bartender(bot))
--- src/cogs/manage.py
+++ src/cogs/manage.py
@@ -2,6 +2,14 @@ from asyncio.windows_events import NULL
 import discord
 from discord.ext import commands
 from libs.database import DataBase
+from urllib.parse import urlparse
+
+def is_url(url):
+  try:
+    result = urlparse(url)
+    return all([result.scheme, result.netloc])
+  except ValueError:
+    return False
 
 class Manage(commands.Cog):
     def __init__(self, bot):
@@ -11,18 +19,22 @@ class Manage(commands.Cog):
     @commands.command()
     async def insert(self, ctx, val, url=None):
         # Check producto registrado
-        sql = """SELECT nombre FROM productos (nombre) WHERE nombre = '{}';""".format(val.lower())
+        sql = """SELECT nombre FROM productos WHERE nombre = '{}';""".format(val.lower())
         self.database.cursor.execute(sql)
         result = self.database.cursor.fetchall()
 
         # Producto registrado + img disponible
         if result != [] and url is not None:
-            sql = """INSERT INTO imagenes (nombre, url) VALUES ('{}', '{}');""".format(val.lower(), url)
-            self.database.cursor.execute(sql)
+            if is_url(url):
+                sql = """INSERT INTO imagenes (nombre, url) VALUES ('{}', '{}');""".format(val.lower(), url)
+                self.database.cursor.execute(sql)
+                await ctx.send("New product: {}".format(val.lower()))
+            else:
+                await ctx.send("Error > Not a well formed url.")
 
         # Producto registrado + imagen no disponible
         elif result != [] and url is None:
-            pass    # EXCEPCION
+            await ctx.send("Product already exist.")
 
         # Producto no registrado + imagen disponible
         elif result == [] and url is not None:
@@ -31,15 +43,31 @@ class Manage(commands.Cog):
             self.database.mydb.commit()
             sql = """INSERT INTO imagenes (nombre, url) VALUES ('{}', '{}');""".format(val.lower(), url)
             self.database.cursor.execute(sql)   # Registra una img para val
+            await ctx.send("New product: {}".format(val.lower()))
 
         # Producto no registrado + imagen no disponible
         else:
             sql = """INSERT INTO productos (nombre) VALUES ('{}');""".format(val.lower())
             self.database.cursor.execute(sql)   # Registra el producto { val }
+            await ctx.send("New product: {}".format(val.lower()))
+
         
         self.database.mydb.commit()
-        await ctx.send(val)
 
+    @commands.command()
+    async def update(self, ctx, val_old, val_new):
+        sql = """SELECT * FROM productos WHERE nombre = '{}';""".format(val_old.lower())
+        self.database.cursor.execute(sql)
+        result = self.database.cursor.fetchall()
+
+        if result != []:
+            sql = """UPDATE productos SET nombre = '{}' WHERE nombre = '{}';""".format(val_new.lower(), val_old.lower())
+            self.database.cursor.execute(sql)
+            self.database.mydb.commit()
+            await ctx.send("Name changed to: {}".format(val_new.capitalize()))
+        else:
+            await ctx.send("Error while changing the product name.")
+    
     @commands.command()
     async def erase(self, ctx, val):
         sql = """DELETE FROM imagenes WHERE id = {};""".format(int(val))
@@ -49,10 +77,17 @@ class Manage(commands.Cog):
 
     @commands.command()
     async def delete(self, ctx, val):
-        sql = """DELETE FROM productos WHERE nombre = '{}';""".format(val.lower())
+        sql = """SELECT * FROM productos WHERE nombre = '{}';""".format(val.lower())
         self.database.cursor.execute(sql)
-        self.database.mydb.commit()
-        await ctx.send(val)
+        result = self.database.cursor.fetchall()
+
+        if result != []:
+            sql = """DELETE FROM productos WHERE nombre = '{}';""".format(val.lower())
+            self.database.cursor.execute(sql)
+            self.database.mydb.commit()
+            await ctx.send("Product deleted: {}".format(val.capitalize()))
+        else:
+            await ctx.send("Error while deleting the product.")
 
     @commands.command()
     async def select(self, ctx, val):
@@ -67,6 +102,8 @@ class Manage(commands.Cog):
             for row in result:
                 data = f'id: {row[0]}, product-name: {row[1]}'
                 await ctx.send(data)
+        else:
+            await ctx.send("Error > No right product selected.")
     
 def setup(bot):
     bot.add_cog(Manage(bot))
