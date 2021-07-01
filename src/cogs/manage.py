e8a76104f065048a426f364d228e729ff1366c2c
Hector Toral <hectortoraltrabajos@gmail.com>
2021-07-01 21:22:35 +0200

    embed sql

--- src/cogs/manage.py
+++ src/cogs/manage.py
@@ -111,8 +111,9 @@ class Manage(commands.Cog):
             result = self.database.cursor.fetchall()
 
             iter = 1
+            max_items = len(result)
             for row in result:
-                if iter == 5:
+                if iter == 5 or iter == max_items:
                     text = str(row[0]) + '.'
                 else:
                     text = str(row[0]) + ', '
