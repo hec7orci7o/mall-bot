97be8ca6e2889a3f5d6d44a11f45ae7a14d48db7
Hector Toral <hectortoraltrabajos@gmail.com>
2021-07-02 17:37:39 +0200

    testing

--- src/cogs/manage.py
+++ src/cogs/manage.py
@@ -1,4 +1,3 @@
-import os
 import discord
 from discord.ext import commands
 from libs.database import DataBase
@@ -10,17 +9,14 @@ class Manage(commands.Cog):
         self.bot = bot
         self.database = DataBase()
 
-    # Check producto registrado
-    async def check(self,ctx, val: str):
+    # sql = f"SELECT count(*) FROM imagenes WHERE nombre = '{val.lower()}';"
+    async def read(self, ctx, sql):
         try:
-            sql = f"SELECT count(*) FROM imagenes WHERE nombre = '{val.lower()}';"
             self.database.cursor.execute(sql)
             result = self.database.cursor.fetchall()
         except self.mysql.connector.Error as err:
             await ctx.send(embed= util.fail(err))
-            
-        print(str(result)[2:-3])
-        return int(str(result)[2:-3])
+        return result
 
     async def write(self, ctx, sql):
         try:
@@ -31,34 +27,37 @@ class Manage(commands.Cog):
 
     @commands.command()
     async def insert(self, ctx, val: str, url: str= ""):
-        result = await self.check(ctx, val)
-        # Nuevo producto
-        if result == 0:
-            if url == "":
-                await self.write(ctx, f"INSERT INTO productos (nombre) VALUES ('{val.lower()}');")
-                await ctx.send(embed= util.success(f"New product: {val.lower()}"))
-            elif util.is_url(url):
-                await self.write(ctx, f"INSERT INTO productos (nombre) VALUES ('{val.lower()}');")
-                await self.write(ctx, f"INSERT INTO imagenes (nombre, url) VALUES ('{val.lower()}', '{url}');")
-                await ctx.send(embed= util.success(f"New product & image for: {val.lower()}"))
-            else:
-                await ctx.send(embed= util.fail("Error, not a well formed url."))
+        result = await self.read(ctx, f"SELECT nombre FROM productos WHERE nombre = '{val.lower()}';")
 
-        # Nueva representacion (img) del producto
-        elif result > 0:
-            if result < 5 and util.is_url(url):
-                await self.write(ctx, f"INSERT INTO imagenes (nombre, url) VALUES ('{val.lower()}', '{url}');")
-                await ctx.send(embed= util.success(f"New product & image for: {val.lower()}"))
-            elif result >= 5:
-                await ctx.send(embed= util.fail("Error, to much imgs for the same product."))
-            else:
-                await ctx.send(embed= util.fail("Error, not a well formed url."))
+        # Producto ya existe
+        if result == val.lower():
+            print("existe")
+            # if result < 5 and util.is_url(url):
+            #     await self.write(ctx, f"INSERT INTO imagenes (nombre, url) VALUES ('{val.lower()}', '{url}');")
+            #     await ctx.send(embed= util.success(f"New product & image for: {val.lower()}"))
+            # elif result >= 5:
+            #     await ctx.send(embed= util.fail("Error, to much imgs for the same product."))
+            # else:
+            #     await ctx.send(embed= util.fail("Error, not a well formed url."))
+
+        # Nuevo producto
+        else:
+            print("no existe")
+            # if url == "":
+            #     await self.write(ctx, f"INSERT INTO productos (nombre) VALUES ('{val.lower()}');")
+            #     await ctx.send(embed= util.success(f"New product: {val.lower()}"))
+            # elif util.is_url(url):
+            #     await self.write(ctx, f"INSERT INTO productos (nombre) VALUES ('{val.lower()}');")
+            #     await self.write(ctx, f"INSERT INTO imagenes (nombre, url) VALUES ('{val.lower()}', '{url}');")
+            #     await ctx.send(embed= util.success(f"New product & image for: {val.lower()}"))
+            # else:
+            #     await ctx.send(embed= util.fail("Error, not a well formed url."))
 
     @commands.command()
     async def update(self, ctx, val_old: str, val_new: str):
-        result = await self.check(ctx, val_old)
+        result = await self.read(ctx, f"SELECT nombre FROM productos WHERE nombre = '{val_old.lower()}';")
 
-        if result > 0:
+        if result != []:
             await self.write(ctx, f"UPDATE productos SET nombre = '{val_new.lower()}' WHERE nombre = '{val_old.lower()}';")
             await ctx.send(embed= util.success(f"Name changed to: {val_new.capitalize()}"))
         else:
@@ -66,12 +65,7 @@ class Manage(commands.Cog):
     
     @commands.command()
     async def delete(self, ctx, val: int):
-        try:
-            sql = f"SELECT * FROM imagenes WHERE id = '{int(val)}';"
-            self.database.cursor.execute(sql)
-            result = self.database.cursor.fetchall()
-        except self.mysql.connector.Error as err:
-                await ctx.send(embed= util.fail(err))
+        result = await self.read(ctx, f"SELECT * FROM imagenes WHERE id = '{int(val)}';")
 
         if result != []:
             await self.write(ctx, f"DELETE FROM imagenes WHERE id = {int(val)};")
@@ -81,9 +75,9 @@ class Manage(commands.Cog):
 
     @commands.command()
     async def clear(self, ctx, val: str):
-        result = await self.check(ctx, val)
+        result = await self.read(ctx, f"SELECT nombre FROM productos WHERE nombre = '{val.lower()}';")
 
-        if result > 0:
+        if result == val.lower():
             await self.write(ctx, f"DELETE FROM productos WHERE nombre = '{val.lower()}';")
             await ctx.send(embed= util.success(f"Product deleted: {val.capitalize()}"))
         else:
@@ -91,29 +85,29 @@ class Manage(commands.Cog):
 
     @commands.command()
     async def select(self, ctx, val: str):
-        result = await self.check(ctx, val)
+        # Comprueba que exista el prodcuto
+        result = await self.read(ctx, f"SELECT nombre FROM productos WHERE nombre = '{val.lower()}';")
 
-        if result > 0:
-            try:
-                sql = f"SELECT id, nombre FROM imagenes WHERE nombre = '{val.lower()}';"
-                self.database.cursor.execute(sql)
-                result = self.database.cursor.fetchall()
-            except self.mysql.connector.Error as err:
-                await ctx.send(embed= util.fail(err))
-
-            iter = 1
-            max_items = len(result)
-            text = ""
-            for row in result:
-                if iter == 5 or iter == max_items:  text += str(row[0]) + '.'
-                else:                               text += str(row[0]) + ', '
-                iter += 1
-
-            embed = discord.Embed(description=f"IDs related to {row[1]}:\n```c++\n{text}```", color=9224068)
-            embed.set_author(name="SQL query", icon_url="https://image.flaticon.com/icons/png/512/2306/2306022.png")
-            embed.set_footer(text="Made with 💘 by Hec7orci7o.", icon_url="https://avatars.githubusercontent.com/u/56583980?s=60&v=4")
+        if result == val.lower():
+            # Comprueba el numero de imagenes que existe para un producto
+            result = int(str(await self.read(ctx, f"SELECT count(*) FROM imagenes WHERE nombre = '{val.lower()}';"))[2:-3])
+            if result == 0:
+                embed = discord.Embed(description=f"IDs related to {val.lower()}:\n```c++\nNo images has been added yet.```", color=9224068)
+                embed.set_author(name="SQL query", icon_url="https://image.flaticon.com/icons/png/512/2306/2306022.png")
+                embed.set_footer(text="Made with 💘 by Hec7orci7o.", icon_url="https://avatars.githubusercontent.com/u/56583980?s=60&v=4")
             
-            await ctx.send(embed=embed)
+            else:
+                result = await self.read(ctx, f"SELECT id, nombre FROM imagenes WHERE nombre = '{val.lower()}';")
+                iter, max_items, text = 1, len(result), ""
+                for row in result:
+                    if iter == 5 or iter == max_items:  text += str(row[0]) + '.'
+                    else:                               text += str(row[0]) + ', '
+                    iter += 1
+
+                embed = discord.Embed(description=f"IDs related to {row[1]}:\n```c++\n{text}```", color=9224068)
+                embed.set_author(name="SQL query", icon_url="https://image.flaticon.com/icons/png/512/2306/2306022.png")
+                embed.set_footer(text="Made with 💘 by Hec7orci7o.", icon_url="https://avatars.githubusercontent.com/u/56583980?s=60&v=4")
+            await ctx.send(embed= embed)
         else:
             await ctx.send(embed= util.fail("No right product selected."))
 
