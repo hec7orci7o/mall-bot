09afa804e50ea64810817956918b5a8c761cb26f
Hector Toral <hectortoraltrabajos@gmail.com>
2021-07-03 00:52:54 +0200

    v2 help

--- src/main.py
+++ src/main.py
@@ -5,22 +5,24 @@ from discord import widget
 from discord import channel
 from discord.embeds import Embed
 from discord.ext import commands
+from discord.ext.commands.core import command
 
 class HelpCommand(commands.HelpCommand):
     def __init__(self):
         super().__init__()
     
     async def send_bot_help(self, mapping):
-        return await super().send_bot_help(mapping)
+        for cog in mapping:
+            await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in mapping[cog]]}')
 
     async def send_cog_help(self, cog):
-        return await super().send_cog_help(cog)
-
-    async def send_group_help(self, group):
-        return await super().send_group_help(group)
+        await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in cog.get_command()]}')
     
     async def send_command_help(self, command):
-        return await super().send_command_help(command)
+        await self.get_destination().send(command.name)
+
+    async def send_group_help(self, group):
+        await self.get_destination().send(f'{group.name}: {[command.name for index, command in enumerate(group.commands)]}')
 
 class MallBot(commands.Bot):
     async def on_ready(self):
