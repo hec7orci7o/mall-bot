f7de5a0ba9e77f442267a7a92bc3a5b703fb5459
Hector Toral <hectortoraltrabajos@gmail.com>
2021-07-05 20:26:38 +0200

    bartender traducciones hechas

--- src/libs/utils.py
+++ src/libs/utils.py
@@ -3,8 +3,6 @@ import discord
 from urllib.parse import urlparse
 from googletrans import Translator
 
-DEST = 'en'
-
 def translate(txt: str, src:str = 'es', dest:str = 'es'):
     translator = Translator()
     traduccion = str(translator.translate(text= txt, src=src, dest= dest).text)
@@ -20,22 +18,22 @@ def is_url(url: str):
 
 def fail(error: str):
     embed = discord.Embed(
-        description= "```c++\n{}```".format(translate(error, dest= DEST)),
+        description= "```c++\n{}```".format(translate(error, dest= 'en')),
         color= int("DE7875", 16)
     )
     embed.set_author(
-        name= translate("Error", dest= DEST),
+        name= translate("Error", dest= 'en'),
         icon_url= "https://www.freeiconspng.com/uploads/x-png-18.png"
     )
     embed.set_footer(
-        text= translate("Hecho con 💘 por Hec7orci7o.", dest= DEST),
+        text= translate("Hecho con 💘 por Hec7orci7o.", dest= 'en'),
         icon_url= "https://avatars.githubusercontent.com/u/56583980?s=60&v=4"
     )
     return embed
 
 def success(success: str):
     embed = discord.Embed(
-        description= "```c++\n{}```".format(translate(success, dest= DEST)),
+        description= "```c++\n{}```".format(translate(success, dest= 'en')),
         color= int("8CBF84", 16)
     )
     embed.set_author(
@@ -43,7 +41,7 @@ def success(success: str):
         icon_url= "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/240px-Flat_tick_icon.svg.png"
     )
     embed.set_footer(
-        text= translate("Hecho con 💘 por Hec7orci7o.", dest= DEST),
+        text= translate("Hecho con 💘 por Hec7orci7o.", dest= 'en'),
         icon_url= "https://avatars.githubusercontent.com/u/56583980?s=60&v=4"
     )
     return embed
